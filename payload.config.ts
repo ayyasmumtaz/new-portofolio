14ba721284126894a7dd5ad15d2c2828724f5265
ayyasmumtaz <yazmumtaz@gmail.com>
2025-12-24 11:18:59 +0700

    Update payload config

--- payload.config.ts
+++ payload.config.ts
@@ -8,7 +8,7 @@ import sharp from 'sharp'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
-import {Experience} from './collections/Experience'
+import { Experience } from './collections/Experience'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -29,6 +29,7 @@ export default buildConfig({
   db: mongooseAdapter({
     url: process.env.DATABASE_URI || '',
   }),
+  serverURL: process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000',
   sharp,
   plugins: [
     // storage-adapter-placeholder
